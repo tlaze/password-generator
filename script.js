cec37b49c381c4d44aab6097ea17384ee3b1b241
Tom Lazore <m>
2021-09-16 00:02:10 -0400

    created generatePassword function

---
+++ script.js
@@ -0,0 +1,32 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// Generate a Password
+function generatePassword(){
+  console.log("Button Clicked");
+  return "Gnerated Password will go here";
+}
+
+
+
+// Steps
+// Make generatePassword function
+// Prompt user for password criteria
+//  a. Between 8 and 128 characters
+//  b. lowercase, uppercase, special characters
+// Validate the input 
+// Generate Password based on criteria
+// Display Password on the page
