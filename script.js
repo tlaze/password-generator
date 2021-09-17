bd9cb3e1649b2c3b2b50a6f6519e8972a1ef5f1f
Tom Lazore <m>
2021-09-16 20:20:39 -0400

    Added array and a variable to determine its length

--- script.js
+++ script.js
@@ -10,13 +10,13 @@
 // Generate Password based on criteria
 // Display Password on the page
 
-
+var passArrayLength = 0;
+var finalPassword = [passArrayLength];
 
 // Generate a Password
 function generatePassword(){
   passwordLength();
 }
-
 
 function passwordLength(){
   
@@ -25,6 +25,8 @@ function passwordLength(){
     var characterCount = parseInt(window.prompt("How long would you like your password?\nChoose Between 8 and 128 Characters"));
     // Repeats prompt until user gives appropriate input
     if(!isNaN(characterCount) && characterCount > 7 && characterCount < 129){
+      passArrayLength = characterCount;
+      console.log(passArrayLength);
       characterQuestions();
     }
     // Validation for length of of password
@@ -33,61 +35,60 @@ function passwordLength(){
   }
   }
   while(isNaN(characterCount) || characterCount < 8 || characterCount > 128);
-  
+} 
 
   
 function characterQuestions(){
 
-    //Asks if user wants to include lowercase characters included   
-    var lowerCaseQuestion = confirm("Would you like your password to contain lowercase characters?");
-    if(lowerCaseQuestion === true){
-      window.alert("Lowercase Characters Included");
-    }
-    else{
-      window.alert("No Lowercase Characters Included");
-    }
+  //Asks if user wants to include lowercase characters included   
+  var lowerCaseQuestion = confirm("Would you like your password to contain lowercase characters?");
+  if(lowerCaseQuestion === true){
+    window.alert("Lowercase Characters Included");
+  }
+  else{
+    window.alert("No Lowercase Characters Included");
+  }
 
-    // Asks if user wants to include uppercase characters included
-    var upperCaseQuestion = confirm("Would you like your password to contain uppercase characters?");
+  // Asks if user wants to include uppercase characters included
+  var upperCaseQuestion = confirm("Would you like your password to contain uppercase characters?");
 
-    if(upperCaseQuestion === true){
-      window.alert("Uppercase Characters Included");
+  if(upperCaseQuestion === true){
+    window.alert("Uppercase Characters Included");
 
-    }
-    else{
-      window.alert("No Uppercase Characters Included");
-    }
+  }
+  else{
+    window.alert("No Uppercase Characters Included");
+  }
 
-    // Asks user if they want numeric characters included
-    var numericQuestion = confirm("Would you like your password to contain numeric characters?");
+  // Asks user if they want numeric characters included
+  var numericQuestion = confirm("Would you like your password to contain numeric characters?");
 
-    if(numericQuestion === true){
-      window.alert("Numeric Characters Included");
+  if(numericQuestion === true){
+    window.alert("Numeric Characters Included");
 
-    }
-    else{
-      window.alert("No Numeric Characters Included");
-    }
+  }
+  else{
+    window.alert("No Numeric Characters Included");
+  }
 
-    // Asks user if they want special characters included
-    var specialCharacterQuestion = confirm("Would you like your password to contain special characters?");
+  // Asks user if they want special characters included
+  var specialCharacterQuestion = confirm("Would you like your password to contain special characters?");
 
-    if(specialCharacterQuestion === true){
-      window.alert("Special Characters Included");
+  if(specialCharacterQuestion === true){
+    window.alert("Special Characters Included");
 
-    }
-    else{
-      window.alert("No Special Characters Included");
+  }
+  else{
+    window.alert("No Special Characters Included");
 
-    }
-    // Repeats prompts until user includes a character type
-    if(lowerCaseQuestion === false && upperCaseQuestion === false && numericQuestion == false && specialCharacterQuestion === false){
-      window.alert("You must include atleast 1 character type.");
-      characterQuestions();
-    }
-    else{
-      return;  //change to generatepassword function
-    }
+  }
+  // Repeats prompts until user includes atleast 1 character type
+  if(lowerCaseQuestion === false && upperCaseQuestion === false && numericQuestion == false && specialCharacterQuestion === false){
+    window.alert("You must include atleast 1 character type.");
+    characterQuestions();
+  }
+  else{
+    return;  //change to generatepassword function
   }
 }
 
@@ -111,4 +112,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
